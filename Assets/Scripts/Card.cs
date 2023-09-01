5fecf2c14302972a7eaf130d30c3d605cdbb33db
ProfL3304 <ProfL3304@lab.aca>
2023-09-01 16:52:35 -0500

    Imagenes de pokemones agregadas

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -6,4 +6,20 @@ public class Card : MonoBehaviour
 {
     public GameObject CardOpen;
     public GameObject CardClose;
+    private bool isClosed = true;
+
+    public void CardOnClick()
+    {
+        if (isClosed == true)
+        {
+            CardClose.SetActive(false);
+            CardOpen.SetActive(true);
+            isClosed = false;
+        }else
+        {
+            CardClose.SetActive(true);
+            CardOpen.SetActive(false);
+            isClosed = true;
+        }
+    }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
+
+public class PokemonCard
+{
+    public string pokemonName;
+    public bool isShown;
+
+    public PokemonCard(string name)
+    {
+        pokemonName = name;
+        isShown = false;
+    }
+}
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private List<PokemonCard> pokemonCardList;
 
-    // Update is called once per frame
-    void Update()
+    private void Start() 
     {
-        
+        pokemonCardList = new List<PokemonCard>();
+
+        PokemonCard card1 = new PokemonCard("pikachu");
+        PokemonCard card2 = new PokemonCard("snorlax");
+        PokemonCard card3 = new PokemonCard("pidgey");
+        PokemonCard card4 = new PokemonCard("jigglypuff");
+        PokemonCard card5 = new PokemonCard("bulbasur");
+        PokemonCard card6 = new PokemonCard("charmander");
+        PokemonCard card7 = new PokemonCard("pidgey");
+        PokemonCard card8 = new PokemonCard("psyduck");
+        PokemonCard card9 = new PokemonCard("pikachu");
+        PokemonCard card10 = new PokemonCard("psyduck");
+        PokemonCard card11 = new PokemonCard("jigglypuff");
+        PokemonCard card12 = new PokemonCard("squirtle");
+        PokemonCard card13 = new PokemonCard("snorlax");
+        PokemonCard card14 = new PokemonCard("charmander");
+        PokemonCard card15 = new PokemonCard("squirtle");
+        PokemonCard card16 = new PokemonCard("bulbasur");
+
+        pokemonCardList.Add(card1);
+        pokemonCardList.Add(card2);
+        pokemonCardList.Add(card3);
+        pokemonCardList.Add(card4);
+        pokemonCardList.Add(card5);
+        pokemonCardList.Add(card6);
+        pokemonCardList.Add(card7);
+        pokemonCardList.Add(card8);
+        pokemonCardList.Add(card8);
+        pokemonCardList.Add(card10);
+        pokemonCardList.Add(card11);
+        pokemonCardList.Add(card12);
+        pokemonCardList.Add(card13);
+        pokemonCardList.Add(card14);
+        pokemonCardList.Add(card15);
+        pokemonCardList.Add(card16);
+
+
+
     }
 }
